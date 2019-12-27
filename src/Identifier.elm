72aa9379ede191ad5cc989cdbba317c3f5d05a01
Matthew Healy <matthew.healy@soundcloud.com>
2019-12-27 01:22:56 +0000

    Parse Identifier from String

--- src/Identifier.elm
+++ src/Identifier.elm
@@ -1,4 +1,7 @@
-module Identifier exposing (Identifier, toString)
+module Identifier exposing ( Identifier
+                           , toString
+                           , parse
+                           )
 
 type alias Identifier = { namespace: String
                         , collection: String
@@ -9,3 +12,17 @@ toString : Identifier -> String
 toString id = id.namespace ++ ":"
               ++ id.collection ++ ":"
               ++ id.identifier
+
+
+parse : String -> Maybe Identifier
+parse str =
+  let
+    components = List.filter (not << String.isEmpty) (String.split ":" str)
+  in
+    case components of
+      [namespace, collection, identifier] ->
+        Just { namespace = namespace
+             , collection = collection
+             , identifier = identifier
+             }
+      _ -> Nothing
--- tests/IdentifierTests.elm
+++ tests/IdentifierTests.elm
@@ -1,30 +1,58 @@
-module IdentifierTests exposing (..)
+module IdentifierTests exposing (suite)
 
 import Expect exposing (Expectation)
 import Test exposing (..)
 
-import Identifier
+import Identifier exposing (Identifier)
+
+-- An unchecked method for constructing a new identifier.
+newIdentifier : String -> String -> String -> Identifier
+newIdentifier n c i = { namespace = n, collection = c, identifier = i }
 
 suite : Test
 suite =
     describe "Identifier"
-      [ describe "toString"
-        [ test "Can convert an Identifier to a String" <|
-          \_ ->
-            let result = Identifier.toString { namespace = "namespace"
-                                             , collection = "collection"
-                                             , identifier = "identifier"
-                                             }
-            in Expect.equal "namespace:collection:identifier" result
-
-        , test "Resulting String is not hardcoded" <|
-          \_ ->
-            let result = Identifier.toString { namespace = "a"
-                                             , collection = "b"
-                                             , identifier = "c"
-                                             }
-            in
-              Expect.equal "a:b:c" result
-
-        ]
+      [ testToString
+      , testParse
       ]
+
+testToString : Test
+testToString =
+  describe "toString"
+    [ test "Can convert an Identifier to a String" <|
+        \_ ->
+          let result = Identifier.toString (newIdentifier "na" "co" "id")
+          in
+            Expect.equal "na:co:id" result
+
+    , test "Resulting String is not hardcoded" <|
+        \_ ->
+          let result = Identifier.toString (newIdentifier "a" "b" "c")
+          in
+            Expect.equal "a:b:c" result
+    ]
+
+testParse : Test
+testParse =
+  describe "parse"
+    [ test "Empty string returns Nothing" <|
+        \_ -> Expect.equal Nothing (Identifier.parse "")
+    , describe "Well-formed identifiers"
+      [ test "are parsed correctly" <|
+        \_ ->
+          let
+            expected = Just (newIdentifier "m" "l" "h")
+            actual = Identifier.parse "m:l:h"
+          in Expect.equal expected actual
+      , test "are not parsed to a hardcoded value" <|
+        \_ ->
+          let
+            expected = Just (newIdentifier "z" "zz" "zzz")
+            actual = Identifier.parse "z:zz:zzz"
+          in Expect.equal expected actual
+      ]
+    , test "Two component identifier returns Nothing" <|
+        \_ -> Expect.equal Nothing (Identifier.parse "a:b")
+    , test "String with an empty component returns nothing" <|
+        \_ -> Expect.equal Nothing (Identifier.parse "a::b")
+    ]
